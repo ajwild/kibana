97cbb38085d2ac2cf7e8999737081a9639f1f2ca
Alex Wild <alex.j.wild@gmail.com>
2020-05-11 22:48:19 +0100

    Remove isEmbedded from chrome service

--- src/core/public/chrome/chrome_service.tsx
+++ src/core/public/chrome/chrome_service.tsx
@@ -105,8 +105,9 @@ export class ChromeService {
    *      reset the visibility whenever the next application is mounted
    *   3. Having "embed" in the query string
    */
-  private initVisibility(application: StartDeps['application'], isEmbedded: boolean) {
+  private initVisibility(application: StartDeps['application']) {
     // Start off the chrome service hidden if "embed" is in the hash query string.
+    const isEmbedded = 'embed' in parse(location.hash.slice(1), true).query;
     this.isForceHidden$ = new BehaviorSubject(isEmbedded);
 
     const appHidden$ = merge(
@@ -137,9 +138,7 @@ export class ChromeService {
     notifications,
     uiSettings,
   }: StartDeps): Promise<InternalChromeStart> {
-    const isEmbedded = 'embed' in parse(location.hash.slice(1), true).query;
-
-    this.initVisibility(application, isEmbedded);
+    this.initVisibility(application);
 
     const appTitle$ = new BehaviorSubject<string>('Kibana');
     const brand$ = new BehaviorSubject<ChromeBrand>({});
@@ -179,8 +178,6 @@ export class ChromeService {
       navLinks,
       recentlyAccessed,
       docTitle,
-
-      isEmbedded,
 
       getHeaderComponent: () => (
         <React.Fragment>
@@ -312,8 +309,6 @@ export interface ChromeStart {
   recentlyAccessed: ChromeRecentlyAccessed;
   /** {@inheritdoc ChromeDocTitle} */
   docTitle: ChromeDocTitle;
-
-  isEmbedded: boolean;
 
   /**
    * Sets the current app's title
